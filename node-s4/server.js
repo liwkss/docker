8b35199c5b59e69b09b52f43ba88e58ba114ea28
Kit <liwkcuhk+gitlab@gmail.com>
2022-07-03 01:23:23 +0800

    node-scrape4

---
+++ node-s4/server.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const webScraper = async () => {
+ const html = await axios.get('https://news.ycombinator.com');
+ const $ = await cheerio.load(html.data);
+ let data = [];
+ $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
+   if (i <= 3) {
+     data.push({
+       title: $(elem).text(),
+       link: $(elem).find('a.storylink').attr('href')
+       })
+     }
+});
+console.log(data);
+}
+webScraper();
--- up.bat
+++ up.bat
@@ -1,3 +1,3 @@
 git add *
-git commit -m "node-scrape3"
+git commit -m "node-scrape4"
 git push  https://github.com/liwkss/docker.git
