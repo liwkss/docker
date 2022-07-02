6b9ccea56db6a50e7a9cfa403010b3b5c0cf1ebc
Kit <liwkcuhk+gitlab@gmail.com>
2022-07-03 00:30:43 +0800

    node-scrape3

--- node-scrape/server.js
+++ node-scrape/server.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 async function scrape() {
-   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
+   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-dev-shm-usage']})
    const page = await browser.newPage()
 
    await page.goto('https://www.thesaurus.com/browse/smart')
