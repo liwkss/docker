48ef4e58edd5be72564a2097b7f628f4da28cb37
Kit <liwkcuhk+gitlab@gmail.com>
2022-07-03 13:25:07 +0800

    scrape-hibor1

---
+++ sh1/server.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer')
+
+async function scrape() {
+   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-dev-shm-usage']})
+   const page = await browser.newPage()
+
+   await page.goto('https://www.hkab.org.hk/hibor/listRates.do?lang=en&Submit=Detail')
+   for(i = 4; i < =11; i++){
+    var eT = await page.waitForSelector("body > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr:nth-child("+i+") > td:nth-child(1)")
+    var eR = await page.waitForSelector("body > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr:nth-child("+i+") > td:nth-child(1)")
+    var text = await page.evaluate(eT => eT.textContent, eT) + await page.evaluate(eR => eR.textContent, eR)
+    console.log(text)
+   }
+   browser.close()
+}
+scrape()
--- up.bat
+++ up.bat
@@ -1,3 +1,3 @@
 git add *
-git commit -m "node-s7"
+git commit -m "scrape-hibor1"
 git push  https://github.com/liwkss/docker.git
