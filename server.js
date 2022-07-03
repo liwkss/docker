c8a6fd40003deba86b43bad43999013b62f18253
Kit <liwkcuhk+gitlab@gmail.com>
2022-07-03 17:46:28 +0800

    scrape-hibor3

---
+++ server.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer')
+
+async function scrape() {
+   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-dev-shm-usage']})
+   const page = await browser.newPage()
+
+   await page.goto('https://www.hkab.org.hk/hibor/listRates.do?lang=en&Submit=Detail')
+
+	const text = await page.evaluate(() => Array.from(document.querySelectorAll("body > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr:nth-child(n+4):nth-last-child(n+3) > td"), element => element.textContent));
+    for(var i=0;i<text.length/2;i++){
+		console.log(text[2*i]+"\t"+text[2*i+1])
+	}
+
+   browser.close()
+}
+scrape()
