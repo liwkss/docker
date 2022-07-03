8f831faa1a29991feba3384de4325e87e25b8dc8
Kit <liwkcuhk+gitlab@gmail.com>
2022-07-03 13:48:48 +0800

    scrape-hibor2

--- sh2/server.js
+++ sh2/server.js
@@ -7,7 +7,7 @@ async function scrape() {
    await page.goto('https://www.hkab.org.hk/hibor/listRates.do?lang=en&Submit=Detail')
    for(i = 4; i <=11; i++){
     var e = await page.waitForSelector("body > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr:nth-child("+i+")")
-    var text = await page.evaluate(eT => e.children[0].textContent +"\t"+ e.children[1].textContent, e)
+    var text = await page.evaluate(e => e.children[0].textContent +"\t"+ e.children[1].textContent, e)
     console.log(text)
    }
    browser.close()
