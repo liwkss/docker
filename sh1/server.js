10f9db93783c590ceccc7c9a7812b0e17aab62f1
Kit <liwkcuhk+gitlab@gmail.com>
2022-07-03 13:27:51 +0800

    scrape-hibor1

--- sh1/server.js
+++ sh1/server.js
@@ -5,7 +5,7 @@ async function scrape() {
    const page = await browser.newPage()
 
    await page.goto('https://www.hkab.org.hk/hibor/listRates.do?lang=en&Submit=Detail')
-   for(i = 4; i < =11; i++){
+   for(i = 4; i <=11; i++){
     var eT = await page.waitForSelector("body > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr:nth-child("+i+") > td:nth-child(1)")
     var eR = await page.waitForSelector("body > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr:nth-child("+i+") > td:nth-child(1)")
     var text = await page.evaluate(eT => eT.textContent, eT) + await page.evaluate(eR => eR.textContent, eR)
