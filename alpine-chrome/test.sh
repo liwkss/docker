f6f74573666ef1a5b1dcf6b384a037db072dc0cb
Kit <liwkcuhk+gitlab@gmail.com>
2022-07-03 12:16:29 +0800

    node-scrape5

--- alpine-chrome/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker container run --rm --entrypoint '' ${IMAGE_NAME:-zenika/alpine-chrome} cat /etc/alpine-release
-docker container run --rm --entrypoint '' ${IMAGE_NAME:-zenika/alpine-chrome} chromium-browser --version
--- alpine-chrome/with-chromedriver/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-DIR=$(cd $(dirname $0) && pwd)
-${DIR}/../test.sh
-docker container run --rm --entrypoint '' ${IMAGE_NAME:-zenika/alpine-chrome:with-chromedriver} chromedriver --version
