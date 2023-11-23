bcf6275ce8c03cbeb4dd132b8a93f62c6b3836e8
Kit <liwkcuhk+gitlab@gmail.com>
2023-11-23 23:28:28 +0800

    sh5

---
+++ sh5/server.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer')
+const express = require('express');
+
+let browser;
+
+
+async function scrape() {
+    var ret="";
+	let page;
+	try{
+		if(!browser) browser = await puppeteer.launch({
+			headless: true,
+			ignoreHTTPSErrors: true,
+			args: ['--no-first-run','--no-sandbox', '--no-zygote','--disable-accelerated-2d-canvas', '--disable-setuid-sandbox','--disable-dev-shm-usage','--disable-gpu'],
+		});
+		page = await browser.newPage()
+
+		await page.goto('https://www.hkab.org.hk/en/rates/hibor')
+
+		const text = await page.evaluate(() => Array.from(document.querySelectorAll("#__nuxt > div > main > div.section_container.for_hibor > div > div > div:nth-child(1) > div > div > div.general_table_cell.last > div"), element => element.textContent));
+		for(var i=0;i<text.length/2;i++){
+			console.log(text[i]);
+			//console.log(text[2*i]+"\t"+text[2*i+1])
+			ret = ret+ text[2*i+1]+",";
+		}
+	}catch(e){
+		console.log(e);
+	}finally{
+		if(page){
+			await page.close();
+		}
+	}
+    return ret;
+}
+
+
+const app = express();
+app.get('/', async(req, res) => {
+  const ret=await scrape()
+  res.send(ret);
+});
+
+app.listen(8080, '0.0.0.0');
+
--- up.bat
+++ up.bat
@@ -1,3 +1,3 @@
 git add *
-git commit -m "sh4"
+git commit -m "sh5"
 git push  https://github.com/liwkss/docker.git
